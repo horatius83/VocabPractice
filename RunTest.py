8cd6ba0cbfcfe3ad851b564de6de0f7f2d75b586
M Monster <horatius83@gmail.com>
2015-10-05 18:29:33 -0500

    RunTest now takes command-line arguments
    RunTest takes command-line arguments:
    .txt files it converts
    .json files it runs as tests
    also added food vocabulary

--- RunTest.py
+++ RunTest.py
@@ -5,20 +5,21 @@
 # automatically convert files ending in .txt to a json file and load that file
 # when encountering a txt file, check for a corresponding json file first
 
+import sys
 from FileManagement import OpenJsonFile, ProcessJsonObj, OutputJsonFile
 from Vocab import Quiz, EightsQuizFunction
 
 def RunTest(filename):
-    print("Opening file: {0}".format(file))
-    jo = OpenJsonFile(file)
+    print("Opening file: {0}".format(filename))
+    jo = OpenJsonFile(filename)
     print("Processing file...")
     joPrime = ProcessJsonObj(jo)
-    print("Making backup of the file to {0}.old".format(file))
-    OutputJsonFile(joPrime,file + '.old')
+    print("Making backup of the file to {0}.old".format(filename))
+    OutputJsonFile(joPrime,filename + '.old')
     print("Starting test...")
     newWords = Quiz(joPrime, EightsQuizFunction)
     print("Recording results...")
-    OutputJsonFile(newWords,file)
+    OutputJsonFile(newWords,filename)
     print("Done.")    
 
 #file = "./KanjiByOldLevel/KanjiQuizLevel3.json"
@@ -29,6 +30,13 @@ def RunTest(filename):
 #file = './Genki2/Chapter15.json'
 #file = './Italian/Italian1.json'
 #file = './Italian/Italian2.json'
-file = './Italian/Italian3.json'
+defaultFile = './Italian/Italian3.json'
 
-RunTest(file)
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        if arg.endswith('.txt'):
+            ConvertFile(arg)
+        elif arg.endswith('.json'):
+            RunTest(arg)
+else:
+    RunTest(defaultFile)

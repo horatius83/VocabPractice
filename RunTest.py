ae6fcf852cfa9071c4df832cf625900a663d6543
horatius83 <horatius83@gmail.com>
2018-02-07 13:08:04 -0600

    studying, fixed recent files issues

--- RunTest.py
+++ RunTest.py
@@ -5,6 +5,7 @@
 # when encountering a txt file, check for a corresponding json file first
 
 import sys
+from typing import Iterable, List
 from FileManagement import OpenJsonFile, ProcessJsonObj, OutputJsonFile
 from Vocab import Quiz, EightsQuizFunction
 from Parsing import ConvertFile
@@ -12,7 +13,8 @@ from os.path import isfile, isdir
 from os import walk
 import os
 
-def RunTest(filename):
+def RunTest(filename: str) -> None:
+    add_recent_file(filename)
     print("Opening file: {0}".format(filename))
     jo = OpenJsonFile(filename)
     print("Processing file...")
@@ -25,24 +27,20 @@ def RunTest(filename):
     OutputJsonFile(newWords,filename)
     print("Done.")    
 
-#file = "./KanjiByOldLevel/KanjiQuizLevel3.json"
-#file = "./Vocabulary/N3.json"
-#file = "./Genki2/Chapter13.json"
-#file = './Genki2/Chapter14.json'
-#file = './Genki2/Chapter13_AtTheBank.json'
-#file = './Genki2/Chapter15.json'
-#file = './Italian/Italian1.json'
-#file = './Italian/Italian2.json'
-#defaultFile = './Italian/Italian3.json'
 recent_files_path = './recent_files.txt'
-def get_recent_files():
+def get_recent_files() -> List[str]:
     with open(recent_files_path,'r') as in_data:
         files = [x for x in in_data.read().split('\n') if x.strip() is not '']
         return files
 
-def update_recent_files(files):
+def update_recent_files(files: Iterable[str]) -> None:
     with open(recent_files_path, 'w') as out_data:
         out_data.write('\n'.join(files))
+
+def add_recent_file(most_recent_file: str) -> None:
+    recent_files = get_recent_files()
+    if most_recent_file not in recent_files:
+        update_recent_files(recent_files[-9:] + [most_recent_file])
 
 if len(sys.argv) > 1:
     for arg in sys.argv[1:]:
@@ -59,7 +57,6 @@ if len(sys.argv) > 1:
                 print(recent_files[:9])
                 print(arg)
                 print([arg])
-                update_recent_files(recent_files[-9:] + [arg])
             RunTest(arg)
         elif isdir(arg):
             (_,_,all_files) = next(walk(arg))
@@ -70,10 +67,9 @@ if len(sys.argv) > 1:
             try:
                 result = int(input('Enter selection: '))
                 RunTest(os.path.join(arg, files[result]))
-            except:
-                pass
+            except Exception as e:
+                print(e)
 else:
-    #RunTest(defaultFile)
     files = []
     if isfile(recent_files_path): 
         files = get_recent_files()
@@ -83,6 +79,7 @@ else:
     selection = input('Enter Selection: ')
     try:
         selection_as_number = int(selection)
-        RunTest(files[selection_as_number])
-    except:
-        pass
+        selected_file = files[selection_as_number]
+        RunTest(selected_file)
+    except Exception as e:
+        print(e)
